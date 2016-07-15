b2e568d1df4553c9c27ac834b0d61d55e0516201
knaseem <knaseem22@gmail.com>
2016-07-15 11:40:04 -0400

    Added CollectionView Controller & Navigation Controller.

---
+++ PhotoCollectionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoCollectionViewController.swift
+//  PhotoBrowser
+//
+//  Created by Khalid Naseem on 15/07/2016.
+//  Copyright © 2016 Khalid Naseem. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class PhotoCollectionViewController: UICollectionViewController {
+    
+    //MARK:  Add constant to match reuse identifier in storyboard:
+    private let reuseIdentifier = "PhotoCell"
+    
+    //MARK:  Section insets:
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    
+
+}
