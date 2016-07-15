cf1e90ded014c973ff2ed1daf4f5781f8ed787ff
knaseem <knaseem22@gmail.com>
2016-07-15 13:51:25 -0400

    Added datasource

--- PhotoCollectionViewController.swift
+++ PhotoCollectionViewController.swift
@@ -18,6 +18,91 @@ class PhotoCollectionViewController: UICollectionViewController {
     //MARK:  Section insets:
     private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
     
+    //MARK: searches in an array, keep track of searches
+    private var searches = [FlickrSearchResults]()
+    //MARK:  flickr is reference to object that will do searching for user.
+    private let flickr = Flickr()
     
-
+    //MARK: photoForIndexPath is a convenience method that will get a specific photo related to an index path in your collection view.
+    func photoForIndexPath(indexPath: NSIndexPath) -> FlickrPhoto {
+        return searches[indexPath.section].searchResults[indexPath.row]
+    }
+    
 }
+
+    //MARK:  This will turn on the Flickr search when user types in search field.
+extension PhotoCollectionViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // 1
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+        textField.addSubview(activityIndicator)
+        activityIndicator.frame = textField.bounds
+        activityIndicator.startAnimating()
+        flickr.searchFlickrForTerm(textField.text!) {
+            results, error in
+            
+            //2
+            activityIndicator.removeFromSuperview()
+            if error != nil {
+                print("Error searching : \(error)")
+            }
+            
+            if results != nil {
+                //3
+                print("Found \(results!.searchResults.count) matching \(results!.searchTerm)")
+                self.searches.insert(results!, atIndex: 0)
+                
+                //4
+                self.collectionView?.reloadData()
+            }
+        }
+        
+        textField.text = nil
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK:// CollectionView DataSource Methods:
+    //1
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return searches.count
+    }
+    
+    //2
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return searches[section].searchResults.count
+    }
+    
+    //3
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PhotoCell", forIndexPath: indexPath) 
+        cell.backgroundColor = UIColor.blackColor()
+        // Configure the cell
+        return cell
+    }
+    
+    //MARK://  CollectionView conforms to pre-built FlowLayout delegate protocol:
+    //1
+    func collectionView(collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                               sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        let flickrPhoto =  photoForIndexPath(indexPath)
+        //2
+        if var size = flickrPhoto.thumbnail?.size {
+            size.width += 10
+            size.height += 10
+            return size
+        }
+        return CGSize(width: 100, height: 100)
+    }
+    
+    //3
+    func collectionView(collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                               insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+}
+
